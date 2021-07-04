968df513b83a3617f7c063a1d297088bda111a36
Monica <monicachadha96@yahoo.co.uk>
2021-07-04 17:46:14 +0100

    session 8 homework: Exception handling

---
+++ lesson_8_atm.py
@@ -0,0 +1,47 @@
+
+def checkpin(user_pin):
+    if user_pin == '1234':
+        print('correct pin')
+        return True
+    else:
+        return False
+
+def user_authentication():
+    tries = 0
+    while tries < 3:
+        user_pin =input('Please enter your pin: ')
+        if checkpin(user_pin):
+            print('Pin Sucess')
+            return True
+        else:
+            print('Incorrect pin')
+            tries = tries + 1
+    raise ValueError('Too many incorrect attempts')
+    return False
+
+def withrawl_amount(balance):
+    while True:
+        withdrawl = int(input('Choose withdrawl amount:'))
+        try:
+            if withdrawl > balance:
+                raise Exception
+            balance = balance - withdrawl
+            print("new balance " + str(balance))
+            return balance
+        except:
+            raise Exception('Your balance is too low')
+        finally:
+            print('Removing your card')
+    return
+
+balance = 100
+
+def atm(user_pin):
+    if user_authentication():
+        withrawl_amount(balance)
+
+def main():
+    print(atm('1234'))
+
+if __name__ == '__main__':
+    main()
