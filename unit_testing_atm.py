fd99ea82fdb632c5b52ed207c8bf0bfac3e812b4
Monica <monicachadha96@yahoo.co.uk>
2021-07-05 08:57:54 +0100

    session 9 homework: unit testing update

--- unit_testing_atm.py
+++ unit_testing_atm.py
@@ -1,19 +1,65 @@
+#breakdown of one of the check pin function
+#user_pin1 = '0000'
+#def checkpin1(user_pin1):
+#    if user_pin1 == '1234':
+#        print('correct pin')
+#        return 'correct'
+#    else:
+#        print('wrong code')
+#        return 'incorrect'
+#
+
+#def main():
+#    print(checkpin1(user_pin1))
+
+#if __name__ == '__main__':
+#    main()
+
+
 import unittest
 from atmcodebreakdown1 import checkpin1
 
 class Testcheckpin1function(unittest.TestCase):
 
-    def correct_pin(self):
+    def test_correct_pin(self):
         expected = 'correct'
         result = checkpin1(user_pin1='1234')
         self.assertEqual(expected, result)
 
-    def wrong_pin(self):
-        expected = 'incorrect'
-        result = checkpin1(user_pin1='0000')
-        self.assertEqual(expected, result)
+
+def test_correct_pin(self):
+    expected = 'incorrect'
+    result = checkpin1(user_pin1='0000')
+    self.assertEqual(expected, result)
+
 
 if __name__ == '__main__':
     unittest.main()
 
-# to run unittests: python3 -m unittest test_red_or_blue.py
+#breakdown of one of the withdrawl function (this would be on a seperate file, submitted for the homework)
+# def withrawl_amount1(balance):
+#         withdrawl = int(input('Choose withdrawl amount:'))
+#         if withdrawl <= balance:
+#             balance = balance - withdrawl
+#             print("new balance " + str(balance))
+#             return balance
+#         else:
+#           print('value too low')
+#           return 'value too low'
+
+from atmcodebreakdown2 import withdrawl_amount1
+
+class Testwithdrawlamount1function(unittest.TestCase):
+
+    def test_under_hundred(self):
+        expected = 50
+        result = withdrawl_amount1(balance=50)
+        self.assertEqual(expected, result)
+
+    def test_wrong_pin(self):
+        expected = 'value too low'
+        result = withdrawl_amount1(balance=110)
+        self.assertEqual(expected, result)
+
+if __name__ == '__main__':
+    unittest.main()
